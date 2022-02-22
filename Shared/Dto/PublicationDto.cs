76b7a01791254be3f754c0c726c07fa105210eb6
Oceane DUQUENNE <oceane.duquenne@capgemini.com>
2022-02-22 17:20:51 +0100

    We can add tags on question. (but we can't send this)

--- Shared/Dto/PublicationDto.cs
+++ Shared/Dto/PublicationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
@@ -16,11 +17,19 @@ namespace CapOverFlow.Shared.Dto
         }
 
         public int PBC_id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name is too long.")]
         public string PBC_title { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Name is too long.")]
         public string PBC_description { get; set; }
+        [Required]
         public bool PBC_resolved { get; set; }
+        [Required]
         public DateTime QST_date { get; set; }
+        [Required]
         public int USR_id { get; set; }
+        [Required]
         public int TYP_id { get; set; }
 
         public TypeDto Type { get; set; }
